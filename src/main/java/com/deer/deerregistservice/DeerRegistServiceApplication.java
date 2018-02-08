798d47c20b0dff19f2b0b3ec75aba88046d94f17
youngdeer <young_deer@sina.com>
2018-02-08 10:29:08 +0800

    git init

---
+++ src/main/java/com/deer/deerregistservice/DeerRegistServiceApplication.java
@@ -0,0 +1,29 @@
+package com.deer.deerregistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class DeerRegistServiceApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeerRegistServiceApplication.class, args);
+	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DeerRegistServiceApplication.class);
+    }
+	
+	@RequestMapping("/info")
+	public String info() {
+		return "this service is for user regist!";
+	}
+}
---
+++ src/test/java/com/deer/deerregistservice/DeerRegistServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deer.deerregistservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeerRegistServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

8f60625a6b07f71574eb67f7fdf291e384d762fb
root <root@youngdeer-P45VJ>
2018-03-10 18:00:07 +0800

    add database pwd encrypt

--- src/test/java/com/deer/deerregistservice/DeerRegistServiceApplicationTests.java
+++ src/test/java/com/deer/deerregistservice/DeerRegistServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.deer.deerregistservice;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,17 @@ public class DeerRegistServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Autowired
+	private  StringEncryptor stringEncryptor;
+
+	
+	@Test
+	public void createDatabasePwd() {
+		//database pwd encrypt,the pwd is q*******(7 numbers)
+		String pwd = stringEncryptor.encrypt("bugaosuni");
+		System.out.println(pwd);
 	}
 
 }

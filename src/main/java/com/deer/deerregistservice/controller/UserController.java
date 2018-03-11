55021ed2c9f9edd20de5f3b33482394d430375bb
youngdeer <young_deer@sina.com>
2018-03-11 14:22:46 +0800

    add swaggerui to show restful api

--- src/main/java/com/deer/deerregistservice/controller/UserController.java
+++ src/main/java/com/deer/deerregistservice/controller/UserController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deer.deerregistservice.entity.UserEntity;
 import com.deer.deerregistservice.service.UserService;
+
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value="/userApi")
@@ -16,7 +19,9 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/listUser")
+	
+	@ApiOperation(value="all user list", notes="all user list")
+	@RequestMapping(value = "/listUser", method = RequestMethod.GET)
 	public List<UserEntity> listUser() {
 		return userService.listUser();
 	}
